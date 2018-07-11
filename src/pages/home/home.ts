9cca66433220f70c7b2c38eae673d2a28081a174
MFT <farihintalib@gmail.com>
2018-07-11 10:25:27 +0800

    .editorconfig .gitignore config.xml ionic.config.json package-lock.json package.json resources\README.md src\app\app.component.ts src\app\app.html src\app\app.module.ts src\app\app.scss src\app\main.ts src\index.html src\manifest.json src\pages\home\home.html src\pages\home\home.scss src\pages\home\home.ts src\service-worker.js src\theme\variables.scss tsconfig.json tslint.json

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  my_url: any;
+  constructor(public navCtrl: NavController,private sanitize: DomSanitizer) {
 
+  }
+  
+
+  urlpaste(){
+    this.my_url = "http://myopensoft.ddns.net:8035/myquest/";
+    return this.sanitize.bypassSecurityTrustResourceUrl(this.my_url);
   }
 
 }
